2bca028cfeb1f032105467e084b584cb7888fdc3
rimka-dev <k.takka01@gmail.com>
2022-04-29 09:31:35 +0200

    new projet

---
+++ src/main/java/fr/ibformation/projetEcoleFormation/ProjetEcoleFormationApplication.java
@@ -0,0 +1,13 @@
+package fr.ibformation.projetEcoleFormation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEcoleFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEcoleFormationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ibformation/projetEcoleFormation/ProjetEcoleFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ibformation.projetEcoleFormation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEcoleFormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
