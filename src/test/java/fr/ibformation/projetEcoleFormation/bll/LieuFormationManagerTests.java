7f6a5affcf2447d6ad6f9c00a29de900b4f200bd
Anael182 <anael.bouchema@gmail.com>
2022-05-02 16:52:26 +0200

    typo

--- src/test/java/fr/ibformation/projetEcoleFormation/bll/LieuFormationManagerTests.java
+++ src/test/java/fr/ibformation/projetEcoleFormation/bll/LieuFormationManagerTests.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import fr.ibformation.projetEcoleFormation.bo.SalleFormation;
 
 @SpringBootTest
-class LieuFormationTests {
+class LieuFormationManagerTests {
 
 	@Autowired
     LieuFormationManager lieuManager;
