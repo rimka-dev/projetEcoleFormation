26f1abe6239f39c4e9ff84d0f38d5f36d1a6ebf5
cgourdon92 <gourdon.celine@gmail.com>
2022-05-03 11:13:40 +0200

    CrudFormationWS + CrudSouSThemeWS + CrudSessionFormationWS

---
+++ src/main/java/fr/ibformation/projetEcoleFormation/ws/CrudFormationWS.java
@@ -0,0 +1,54 @@
+package fr.ibformation.projetEcoleFormation.ws;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.ibformation.projetEcoleFormation.bll.FormationException;
+import fr.ibformation.projetEcoleFormation.bll.FormationManager;
+import fr.ibformation.projetEcoleFormation.bo.Formation;
+
+@RestController
+@RequestMapping("/WS")
+public class CrudFormationWS {
+
+	@Autowired
+	private FormationManager formationManager;
+	
+	
+	@GetMapping("/formation")
+	public List<Formation> getFormations(){
+		return formationManager.getListeFormations();
+	}
+	
+	@GetMapping("/formation/{id}")
+	public Formation getFormationById(@PathVariable("id") Integer id){
+		return formationManager.getFormationById(id);
+	}
+	
+	@PostMapping("/formation")
+	public Formation addFormation(@RequestBody Formation formation) {
+		formationManager.addFormation(formation);
+		return formation;
+	}
+	
+	@PutMapping("/formation")
+	public Formation modifyFormation(@RequestBody Formation formation) {
+		formationManager.modifyFormation(formation);
+		return formation;
+	}
+	
+	@DeleteMapping("/formation/{id}")
+	public void deleteFormationById(@PathVariable("id") Integer id) throws FormationException {
+		formationManager.deleteFormationById(id);
+	}
+	
+}
---
+++ src/main/java/fr/ibformation/projetEcoleFormation/ws/CrudSessionFormationWS.java
@@ -0,0 +1,57 @@
+package fr.ibformation.projetEcoleFormation.ws;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.ibformation.projetEcoleFormation.bll.FormationException;
+import fr.ibformation.projetEcoleFormation.bll.FormationManager;
+import fr.ibformation.projetEcoleFormation.bo.Formation;
+import fr.ibformation.projetEcoleFormation.bo.SessionFormation;
+import fr.ibformation.projetEcoleFormation.bo.SousThemeFormation;
+
+@RestController
+@RequestMapping("/WS")
+public class CrudSessionFormationWS {
+
+	@Autowired
+	private FormationManager formationManager;
+	
+	
+	@GetMapping("/session-formation")
+	public List<SessionFormation> getSessionFormations(){
+		return formationManager.getListeSessionsFormation();
+	}
+	
+	@GetMapping("/session-formation/{id}")
+	public void getSessionFormationById(@PathVariable("id") Integer id) throws FormationException {
+		formationManager.getSessionFormationById(id);
+	}
+	
+	
+	@PostMapping("/session-formation")
+	public SessionFormation addSessionFormation(@RequestBody SessionFormation sessionFormation) {
+		formationManager.addSessionFormation(sessionFormation);
+		return sessionFormation;
+	}
+	
+	@PutMapping("/session-formation")
+	public SessionFormation modifySessionFormation(@RequestBody SessionFormation sessionFormation) {
+		formationManager.modifySessionFormation(sessionFormation);
+		return sessionFormation;
+	}
+	
+	@DeleteMapping("/session-formation/{id}")
+	public void deleteSessionFormationById(@PathVariable("id") Integer id) throws FormationException {
+		formationManager.deleteSessionFormationById(id);
+	}
+	
+}
