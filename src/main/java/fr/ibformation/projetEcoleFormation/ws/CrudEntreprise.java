6c67fa9f5a4745200e51a2a33b0bdca5025e6919
rimka-dev <k.takka01@gmail.com>
2022-05-03 18:01:36 +0200

    WebService fini

---
+++ src/main/java/fr/ibformation/projetEcoleFormation/ws/CrudEntreprise.java
@@ -0,0 +1,56 @@
+package fr.ibformation.projetEcoleFormation.ws;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.ibformation.projetEcoleFormation.bll.LieuException;
+import fr.ibformation.projetEcoleFormation.bll.LieuFormationManager;
+import fr.ibformation.projetEcoleFormation.bo.EntrepriseClient;
+
+@RestController
+@RequestMapping("/WS")
+public class CrudEntreprise {
+	@Autowired
+	private LieuFormationManager manager;
+	
+	@GetMapping("/entreprise")
+	public List<EntrepriseClient> getAllEntreprise(){
+		return manager.getAllEntreprise();
+		
+	}
+	
+	@GetMapping("/entreprise/{id}")
+	public EntrepriseClient getEntrepriseClientById(@PathVariable("id") Integer id) {
+		return manager.getEntrepriseById(id);
+	}
+	
+	@PostMapping("/entreprise")
+	public EntrepriseClient addEntrepriseClient(@RequestBody EntrepriseClient entreprise ) {
+		 manager.addEntreprise(entreprise);
+		 return entreprise;
+		
+	}
+	
+	@PutMapping("/entreprise")
+	public EntrepriseClient modifayEntrepriseClient(@RequestBody EntrepriseClient entreprise ) {
+		manager.updateEntreprise(entreprise);
+		return entreprise;
+		
+	}
+	
+	@DeleteMapping("/entreprise/{id}")
+	public void deleteEntrepriseClient(@PathVariable("id") Integer id) throws LieuException {
+		
+		manager.deleteEntrepriseById(id);
+	}
+
+}
