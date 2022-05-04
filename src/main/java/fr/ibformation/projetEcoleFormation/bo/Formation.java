40b2f711a84ae608f279192bdf672b4b54653020
Anael182 <anael.bouchema@gmail.com>
2022-05-04 10:06:36 +0200

    création formulaire connexion

--- src/main/java/fr/ibformation/projetEcoleFormation/bo/Formation.java
+++ src/main/java/fr/ibformation/projetEcoleFormation/bo/Formation.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
