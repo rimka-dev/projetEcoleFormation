7bfcc448182e28d2d30a0094bbf7414b2a7c71dc
rimka-dev <k.takka01@gmail.com>
2022-05-04 14:53:48 +0200

    @CorssOrigin

--- src/main/java/fr/ibformation/projetEcoleFormation/ws/CrudEntreprise.java
+++ src/main/java/fr/ibformation/projetEcoleFormation/ws/CrudEntreprise.java
@@ -3,6 +3,7 @@ package fr.ibformation.projetEcoleFormation.ws;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.ibformation.projetEcoleFormation.bll.LieuException;
 import fr.ibformation.projetEcoleFormation.bll.LieuFormationManager;
 import fr.ibformation.projetEcoleFormation.bo.EntrepriseClient;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/WS")
 public class CrudEntreprise {
