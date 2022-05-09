15950defe9aa9d8819dbd8d2ccb030c9f8d29983
Anael182 <anael.bouchema@gmail.com>
2022-05-09 20:36:58 +0200

    Update SessionFormation.java

--- src/main/java/fr/ibformation/projetEcoleFormation/bo/SessionFormation.java
+++ src/main/java/fr/ibformation/projetEcoleFormation/bo/SessionFormation.java
@@ -4,12 +4,7 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
