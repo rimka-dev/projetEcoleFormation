0f45217375a2fd189ceb4492079b612350cf99ab
Anael182 <anael.bouchema@gmail.com>
2022-05-10 09:38:30 +0200

    Create UserSessionWS.java

---
+++ src/main/java/fr/ibformation/projetEcoleFormation/ws/UserSessionWS.java
@@ -0,0 +1,21 @@
+package fr.ibformation.projetEcoleFormation.ws;
+
+
+import fr.ibformation.projetEcoleFormation.bo.Stagiaire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class UserSessionWS {
+
+    @Autowired
+    private Stagiaire user;
+
+    @GetMapping("/session")
+    public Stagiaire getUserSession() {
+        return user;
+    }
+}
